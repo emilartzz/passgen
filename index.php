9d8d6435490c3d19db132c1622c0923e947be2b0
Emil W <emilwarelius@gmail.com>
2021-01-31 22:41:20 +0100

    Main files added.

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Signika+Negative:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/main.css">
+
+    <title>Passgen.tech - Random Password Generator</title>
+
+    <meta name="title" content="Passgen.tech">
+    <meta name="description" content="Generate a new random password.">
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://passgen.tech/">
+    <meta property="og:title" content="Passgen.tech">
+    <meta property="og:description" content="Generate a new random password.">
+    <meta property="og:image" content="./img/passgen.tech.png">
+    <meta property="og:image" content="./img/passgen.tech.png">
+    <meta property="og:image:width" content="1200px">
+    <meta property="og:image:height" content="628px">
+
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://passgen.tech/">
+    <meta property="twitter:title" content="Passgen.tech">
+    <meta property="twitter:description" content="Generate a new random password.">
+    <meta property="twitter:image" content="./img/passgen.tech.png">
+
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="brand">
+            <h1 id="h1">Pass.it</h1>
+            <h2>Random password generator </h2>
+        </div>
+
+
+        <div class="passgen">
+
+            <div class="main_passgen">
+                
+                <label for="outPass">Your Password :</label>
+
+                <div class="generator">
+                    <button title="Hide/Show password" alt="Hide/Show password" id="passVis" ><i id="passVisEye" class="fas fa-eye-slash"></i></button>
+                    <input type="password" name="outPass" id="outPass" placeholder="secret password">
+                    <button title="Copy password" alt="Copy password" id="copyPass"><i class="fas fa-copy"></i></button>
+                </div>
+            </div>
+
+            <div class="settings">
+                <div class="length">
+                    <label for="length_selector" id="lblLength">Length:</label>
+                    <select name="length_selector" id="length_selector" >
+                        <option value="8">8 character</option>
+                        <option value="12" selected="selected">12 character</option>
+                        <option value="18">18 character</option>
+                        <option value="20">20 character</option>
+                        <option value="24">24 character</option>
+                        <option value="28">28 character</option>
+                        <option value="32">32 character</option>
+                        <option value="0">Custom Value</option>
+                    </select>
+                    <input type="number" name="customLength" id="customLength">
+                </div>
+
+                <div id="char_options">
+                    <label class="checkbox_container special">Include Symbols
+                        <input type="checkbox" name="specialChars" id="specialChars" checked>
+                        <span class="checkmark"></span>
+                      </label>
+                      <label class="checkbox_container uppercase">Uppercase Characters
+                        <input type="checkbox" name="upperChars" id="upperChars" checked>
+                        <span class="checkmark"></span>
+                      </label>
+                      <label class="checkbox_container lowercase">Lowercase Characters
+                        <input type="checkbox" name="lowerChars" id="lowerChars" checked>
+                        <span class="checkmark"></span>
+                      </label>
+                </div>
+
+                <div id="generateButton">
+                    <button id="btnGenerator">Generate</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" integrity="sha512-WNLxfP/8cVYL9sj8Jnp6et0BkubLP31jhTG9vhL/F5uEZmg5wEzKoXp1kJslzPQWwPT1eyMiSxlKCgzHLOTOTQ==" crossorigin="anonymous"></script>
+    <script src="./js/app.js"></script>
+    
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,108 @@
+var passVis = $('#passVis');
+var copyPass = $('#copyPass');
+var eye = $('#passVisEye');
+var password = $('#outPass');
+
+var selectedOption = $('#length_selector');
+
+var btnGen = $('#btnGenerator');
+
+var specialChars = $('#specialChars');
+var lowerChars = $('#lowerChars');
+var upperChars = $('#upperChars');
+
+// SHOW/HIDE PASS
+
+passVis.click(function() {
+    eye.removeClass('fas fa-eye-slash').addClass( "fas fa-eye" );
+    password.prop('type','text');
+  });
+
+passVis.mouseup(function() {
+    eye.removeClass('fas fa-eye').addClass( "fas fa-eye-slash" );
+    password.prop('type','password');
+  });
+
+passVis.mouseleave(function() {
+    eye.removeClass('fas fa-eye').addClass( "fas fa-eye-slash" );
+    password.prop('type','password');
+  });
+
+// GENERATE BUTTON FUNCTION
+  // SPECIAL CHARS - LOWER CHARS - UPPER CHARS 
+
+btnGen.click(function (){
+
+        // ✓ ✓ ✓
+        if (specialChars.prop("checked") == true && lowerChars.prop("checked") == true && upperChars.prop("checked") == true) {
+            charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$@^<>,.:;~`/'!?}{&[|]";
+        }
+        // X X X
+        else if (specialChars.prop("checked") == false && lowerChars.prop("checked") == false && upperChars.prop("checked") == false) {
+            alert("Select atleast one option");
+        }
+        // X ✓ ✓
+        else if (specialChars.prop("checked") == false && lowerChars.prop("checked") == true && upperChars.prop("checked") == true) {
+            charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        // X X ✓
+        else if (specialChars.prop("checked") == false && lowerChars.prop("checked") == false && upperChars.prop("checked") == true) {
+            charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        // ✓ ✓ X
+        else if (specialChars.prop("checked") == true && lowerChars.prop("checked") == true && upperChars.prop("checked") == false) {
+            charset = "0123456789abcdefghijklmnopqrstuvwxyz#$@^<>,.:;~`/'!?}{&[|]";
+        }
+        // ✓ X X
+        else if (specialChars.prop("checked") == true && lowerChars.prop("checked") == false && upperChars.prop("checked") == false) {
+            charset = "0123456789#$@^<>,.:;~`/'!?}{&[|]";
+        }
+        // X ✓ X
+        else if (specialChars.prop("checked") == false && lowerChars.prop("checked") == true && upperChars.prop("checked") == false) {
+            charset = "0123456789abcdefghijklmnopqrstuvwxyz";
+        }
+
+        if (selectedOption.val() == '0'){
+            var length = $('#customLength').val()
+        }
+        else{
+            var length = selectedOption.val()
+        }
+
+        var retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+
+    password.val(retVal);
+    console.warn(retVal);
+});
+
+// COPY BUTTON
+
+$('#copyPass').click(function() {
+    password.prop('type','text');
+    $('#outPass').select();
+    document.execCommand("copy");
+    
+    password.prop('type','password');
+  });
+
+
+  // ON SELECTOR CHANGE LENGTH
+$('#length_selector').on('change', function() {
+    if (selectedOption.val() == '0'){
+        $('#customLength').css('display', 'inline-block');
+    }
+    else{
+        $('#customLength').css('display', 'none');
+    }
+  });
+
+  // CLICK TO GO HOME
+
+  $('#h1').click(function (){
+
+    window.location.href = "./index.html";
+
+  });
